10fa5b860c4cd2201c773e90e670815356019c7c
Deeq <s1227239@student.windesheim.nl>
2026-09-21 23:54:37 +0200

    setup pins voor LEDs en knoppen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-const int LAMP_PIN = 2;
-const int KNOP_PIN = 3;
+const int KNOP1_PIN = 5;
+const int KNOP2_PIN = 6;
+
+const int LED1 = 2;
+const int LED2 = 3;
+const int LED3 = 4;
+const int LED4 = 7;
+
+const int LED_PINS[] = {LED1, LED2, LED3, LED4};
+
 void setup() {
-    pinMode(LAMP_PIN, OUTPUT);
-    pinMode(KNOP_PIN, INPUT_PULLUP);
+    Serial.begin(9600);
+    for (int i = 0; i < 4; i++) {
+        pinMode(LED_PINS[i], OUTPUT);
+    }
+    pinMode(KNOP1_PIN, INPUT_PULLUP);
+    pinMode(KNOP2_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-    int knopStatus = digitalRead(KNOP_PIN);
+    int knopStatus = digitalRead(KNOP1_PIN);
 
     if (knopStatus == LOW) {
-        digitalWrite(LAMP_PIN, HIGH);
+        digitalWrite(LED_PINS[0], HIGH);
     } else {
-        digitalWrite(LAMP_PIN, LOW);
+        digitalWrite(LED_PINS[0], LOW);
     }
 }
